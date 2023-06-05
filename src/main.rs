1cac6eca71af2eeffc24e8ce536e548db4270441
Stefan Gatu <stefan.gatu@gmail.com>
2023-06-06 00:24:30 +0100

    Added User struct with bcrypt password encryption

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use] extern crate rocket;
 use rocket::serde::{Serialize, Deserialize, json::Json};
 
+mod domain;
+use domain::model::User;
+
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Message<'t> {
@@ -8,12 +11,12 @@ struct Message<'t> {
     ok: bool
 }
 #[get("/")]
-fn index<'a>() -> Json<Message<'a>> {
+async fn index<'a>() -> Json<String> {
     let message = Message {
         description: "Hello world!",
         ok: true
     };
-    Json(message)
+    Json(message.description.to_string())
 }
 #[get("/hola")]
 fn hola<'a>() -> Json<Message<'a>> {
@@ -23,9 +26,17 @@ fn hola<'a>() -> Json<Message<'a>> {
     };
     Json(message)
 }
-
+#[get("/user")]
+fn get_user() -> Json<User> {
+    let user = User::new(
+        "Stefan", 
+        "stefan.gatu@gmail.com", 
+        "abc"
+    );
+    Json(user)
+}
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index, hola])
+    .mount("/", routes![index, hola, get_user])
 }
